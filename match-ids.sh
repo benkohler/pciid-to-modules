b76f743d9987a03e17aa0951fa5eefc1fd1900c4
Ben Kohler <bkohler@gentoo.org>
2020-04-07 08:05:49 -0500

    Allow for EOL after ID

--- match-ids.sh
+++ match-ids.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "PCI Matches:"
-for i in $(lspci -n | sed -E 's/.* ([0-9a-fA-F]{4}\:[0-9a-fA-F]{4}) .*/\1/')
+for i in $(lspci -n | sed -E 's/.* ([0-9a-fA-F]{4}\:[0-9a-fA-F]{4}).*/\1/')
 do
 	grep -i ${i} pci.map
 done
